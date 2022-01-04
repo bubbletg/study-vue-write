1e08988b70b2e662e75211a7aab5cc57b837c5f2
BubbleTg <tg_z@qq.com>
2022-01-04 21:52:24 +0800

    ✨ feat: 完成模版编译，完成render 函数

---
+++ src/compiler/index.js
@@ -0,0 +1,93 @@
+import { parserHTML } from "./parser/index"
+
+const defaultTagRE = /\{\{((?:.|\r?\n)+?)\}\}/g // 匹配 {{ }}
+
+/**
+ * 把属性转换为字符串
+ * @param {*} attrs
+ * @returns
+ */
+function genProps(attrs) {
+  let str = ""
+  for (let i = 0; i < attrs.length; i++) {
+    let attr = attrs[i]
+    if (attr.name === "style") {
+      // style="color:red;fontSize:14px;" => {style:{color:'red'}}
+      let obj = {}
+      attr.value.split(";").forEach(item => {
+        let [key, value] = item.split(":")
+        obj[key] = value
+      })
+      attr.value = obj
+    }
+    str += `${attr.name}:${JSON.stringify(attr.value)},`
+  }
+  return `{${str.slice(0, -1)}}`
+}
+
+/**
+ * 处理子元素模版
+ * @param {*} el
+ * @returns
+ */
+function genChildren(el) {
+  let children = el.children
+  if (children && children.length > 0) {
+    return `${children.map(c => gen(c)).join(",")}`
+  } else {
+    return false
+  }
+}
+
+/**
+ * 处理内容
+ * @param {*} node
+ */
+function gen(node) {
+  if (node.type === 1) {
+    return generate(node)
+  } else {
+    let text = node.text
+
+    let tokens = []
+    let match, index
+    let lastIndex = (defaultTagRE.lastIndex = 0)
+    while ((match = defaultTagRE.exec(text))) {
+      index = match.index
+      if (index > lastIndex) {
+        tokens.push(JSON.stringify(text.slice(lastIndex, index)))
+      }
+      tokens.push(`_s(${match[1].trim()})`)
+      lastIndex = index + match[0].length
+    }
+    if (lastIndex < text.length) {
+      tokens.push(JSON.stringify(text.slice(lastIndex)))
+    }
+    return `_v(${tokens.join("+")})`
+  }
+}
+
+/**
+ * 生成一个字符串模版
+ * @param {*} el
+ * @returns
+ */
+function generate(el) {
+  let children = genChildren(el)
+
+  let code = `_c("${el.tag}",${
+    el.attrs.length > 0 ? genProps(el.attrs) : "undefined"
+  }${children ? `,${children}` : ""})
+  `
+  return code
+}
+
+export function compileToFunction(template) {
+  // 解析HTML，变为 AST语法树
+  let root = parserHTML(template)
+
+  // 将 ast 语法树 转换成 字符穿 render 函数模版
+  let code = generate(root)
+  let renderFn = new Function(`with(this){return ${code}}`)
+  return renderFn
+}
