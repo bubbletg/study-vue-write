0f9c0bfb1bc7b7bf1144dfddb7c8078996ca8416
BubbleTg <tg_z@qq.com>
2022-01-06 17:51:53 +0800

    ✨ feat: 对象依赖收集

--- src/observer/watcher.js
+++ src/observer/watcher.js
@@ -1,3 +1,5 @@
+import { pushTarget, popTarget } from "./dep"
+
 export default class Watcher {
   constructor(vm, exprOrFn, callback, options) {
     this.vm = vm
@@ -5,10 +7,31 @@ export default class Watcher {
     this.callback = callback
     this.options = options
 
-    this.getter = exprOrFn
+    this.depsId = new Set() // 保存Dep 的id
+    this.deps = [] // 保存 dep
+    this.getter = exprOrFn  // 
     this.get()
   }
   get() {
+    pushTarget(this) // 把当前watcher 存起来
     this.getter()
+    popTarget()
+  }
+  update() {
+    this.get()
+  }
+  /**
+   * 当前watcher 添加一个 dep ,同时这个 dep 也添加上 这个watcher
+   * @param {*} dep 
+   */
+  addDep(dep) {
+    // watcher 里不能放重复的dep,Dep 不能放重复的watcher
+    let id = dep.id
+    // 判断当前 watcher 是否保存了dep
+    if (!this.depsId.has(id)) {
+      this.depsId.add(id)
+      this.deps.push(dep)
+      dep.addSub(this)
+    }
   }
 }
