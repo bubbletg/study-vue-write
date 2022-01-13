14364d4da0c9e2d2fed6fe1cf98da73f34143b57
BubbleTg <tg_z@qq.com>
2022-01-13 23:07:58 +0800

    ✨ feat: 完成diff算法

--- src/initGlobalAPI/assets.js
+++ src/initGlobalAPI/assets.js
@@ -3,7 +3,7 @@ import {ASSETS_TYPE} from './const'
 export default function initAssetRegisters(Vue){
     ASSETS_TYPE.forEach(type=>{ // 
         Vue[type] = function (id,definition) {
-            if(type === 'component'){
+            if (type === 'component') {
                 // 注册全局组件
                 // 使用extend 方法 将对象变成构造函数
                 // 子组件可能也有这个VueComponent.component方法
