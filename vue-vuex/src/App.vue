c2d674f0a58b177eb16ae30074e788457cb41652
BubbleTg <tg_z@qq.com>
2022-01-16 23:00:13 +0800

    init: init

---
+++ vue-vuex/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div>我得名字： {{ $store.getters.getName }}</div>
+    <div>我得----： {{ $store.state.age }}</div>
+    <button @click="$store.commit('changeName','这是新名字')">同步更新,年纪加 11</button>
+    <button @click="$store.dispatch('changeAge')">异步更新</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      storepro: this.$store,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-vuex/src/store/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  // 组件状态
+  state: {
+    age: '100',
+    name: 'BubbleTg',
+  },
+  // 获得属性
+  getters: {
+    getAge(state) {
+      return state.age + 1000;
+    },
+
+    // 是一个计算属性
+    getName(state) {
+      return `${state.name}1111111`;
+    },
+  },
+  // 唯一改变状态的方法
+  mutations: {
+    changeName(state, name) {
+      state.name = name;
+      state.age += 11;
+    },
+  },
+
+  // 通过 actions 可以发起请求
+  actions: {
+    changeAge({ commit }) {
+      setTimeout(() => {
+        // 调用  changeName 方法，传递的参数是 BubbleTg
+        commit('changeName', 'BubbleTg');
+      }, 1000);
+    },
+  },
+  modules: {},
+});
